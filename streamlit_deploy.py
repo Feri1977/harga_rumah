443415361d135f1d8f3dcd32c7debf78b17dc5f1
Feri1977 <162080839+Feri1977@users.noreply.github.com>
2026-08-20 11:39:26 +0700

    Add files via upload

--- streamlit_deploy.py
+++ streamlit_deploy.py
@@ -7,14 +7,11 @@ model = joblib.load('gbr_model.joblib')
 
 st.title('Prediksi Model Machine Learning')
 
-# Buat form input untuk user (sesuaikan dengan fitur dataset Anda)
-fitur_1 = st.number_input('Input Fitur 1')
-fitur_2 = st.number_input('Input Fitur 2')
-
-if st.button('Prediksi'):
-    # Format input menjadi DataFrame agar sesuai dengan format saat training
-    input_data = pd.DataFrame({'NamaKolom1': [fitur_1], 'NamaKolom2': [fitur_2]})
-    
-    # Eksekusi prediksi
-    hasil = model.predict(input_data)
-    st.success(f'Hasil Prediksi: {hasil[0]}')
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json['data']  # Mengambil data dari request JSON
+    prediction = model.predict(data)  # Melakukan prediksi (harus dalam bentuk 2D array)
+    return jsonify({'prediction': prediction.tolist()})
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
